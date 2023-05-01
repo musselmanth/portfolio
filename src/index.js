99df582dc31e667bd5be83ab440ca85b4e76fdbf
Thomas Musselman <tmusselman@gmail.com>
2023-05-01 13:54:49 -0600

    setup deployment scripts

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Router><App /></Router>
+    <Router basename="/react-remake"><App /></Router>
   </React.StrictMode>
 );
 
