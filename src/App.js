bcf2a9e07802b8c0a428809e98b9c6751d19fc34
Thomas Musselman <tmusselman@gmail.com>
2023-05-04 14:15:28 -0600

    change scroll amt

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
-import NavBar from './NavBar/NavBar.js';
-import Home from './pages/about/AboutPage.js';
+import './App.css'
+import NavBar from './NavBar/NavBar.js'
+import Home from './pages/about/AboutPage.js'
 import Blog from './pages/blog/BlogPage.js'
-import { Routes, Route} from 'react-router-dom';
-import Portfolio from './pages/portfolio/PortfolioPage';
-import Resume from './pages/resume/ResumePage';
-import useWindowResizeThreshold from './hooks/useWindowResizeThreshold';
-import useWindowScrolled from './hooks/useWindowScrolled';
-import Footer from './Footer/Footer';
+import { Routes, Route} from 'react-router-dom'
+import Portfolio from './pages/portfolio/PortfolioPage'
+import Resume from './pages/resume/ResumePage'
+import useWindowResizeThreshold from './hooks/useWindowResizeThreshold'
+import useWindowScrolled from './hooks/useWindowScrolled'
+import Footer from './Footer/Footer'
 import NotFound from './NotFound'
 
-const MAX_MOBILE_WIDTH = 890;
-const SCROLLED_AMT = 120
+const MAX_MOBILE_WIDTH = 890
+const SCROLLED_AMT = 80
 
 const App = () => {
   const isMobileSize = useWindowResizeThreshold(MAX_MOBILE_WIDTH)
