c6f36a003a370c61a28f7cbff6d2d194d4c174e4
Thomas Musselman <tmusselman@gmail.com>
2023-05-03 23:33:01 -0600

    add devto blog posts

--- src/pages/blog/BlogPage.js
+++ src/pages/blog/BlogPage.js
@@ -6,13 +6,32 @@ const DEV_TO_URL = 'https://dev.to/api/articles?username=musselmanth'
 const BlogPage = (props) => {
   const { isLoading, apiData, serverError } = useFetch(DEV_TO_URL)
 
-  console.log(apiData)
-
   return (
     <>
       <h1>Blog</h1>
-      <h3>View and Engage with my posts on <a href="https://dev.to/musselmanth">DEV.to</a></h3>
-      <h3>Recent Posts:</h3>
+      <div className="posts-container">
+        { isLoading && <h3 style={{textAlign: "center"}}>Loading...</h3>}
+        { serverError && <h3 style={{textAlign: "center"}}>There was an error retrieving the posts.</h3> }
+        { apiData !== null && apiData.map((post, i) => {
+          return (
+            <div className="post" key={`post-${i}`} >
+              <a className="post-card" href={post.url}>
+                <img src={post.social_image} className="post-image" />
+                <div className="post-body">
+                  <h3 className="post-title">{post.title}</h3>
+                  <p className="post-description">{post.description}</p>
+                  <div className="post-bottom">
+                    <div className="post-date">{new Date(post.published_at).toLocaleDateString('en-us', { year:"numeric", month:"short", day:"numeric"})}</div>
+                    <div className="post-comments">Comments: {post.comments_count}</div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          )
+        })}
+
+
+      </div>
     </>
   )
 }
