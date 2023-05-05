507f77e65946e9413622f1850f5fc7e4201e27b5
Thomas Musselman <tmusselman@gmail.com>
2023-05-04 23:52:38 -0600

    reorder skills

--- src/pages/portfolio/projectsData.js
+++ src/pages/portfolio/projectsData.js
@@ -33,7 +33,7 @@ const projectsList = [
             "svgString": `<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>NGINX</title><path d="M12 0L1.605 6v12L12 24l10.395-6V6L12 0zm6 16.59c0 .705-.646 1.29-1.529 1.29-.631 0-1.351-.255-1.801-.81l-6-7.141v6.66c0 .721-.57 1.29-1.274 1.29H7.32c-.721 0-1.29-.6-1.29-1.29V7.41c0-.705.63-1.29 1.5-1.29.646 0 1.38.255 1.83.81l5.97 7.141V7.41c0-.721.6-1.29 1.29-1.29h.075c.72 0 1.29.6 1.29 1.29v9.18H18z"/></svg>`
         }
       ],
-      "description": "<p>You're looking at it!</p><p>My portfolio is built with React with a small Express backend for serving up project data. The components and styling are built from scratch. I used the project to improve my CSS skills and focused on making the CSS simple but dynamic for different screen sizes.</p><p>The react app also consumes an external API from dev.to for my blog posts.</p><p>Due to the minimal size and usage of the application, both the back-end and front-end are hosted on a single Digital Ocean droplet running Ubuntu with an NGINX server.</p>"
+      "description": "<p>You're looking at it!</p><p>My portfolio is built with React and a small Express backend for serving up project data. The components and styling are built from scratch. I used the project to improve my CSS skills and focused on making the CSS simple but dynamic for different screen sizes.</p><p>The react app also consumes an external API from dev.to for my blog posts.</p><p>Due to the minimal size and usage of the application, both the back-end and front-end are hosted on a single Digital Ocean droplet running Ubuntu with an NGINX server.</p>"
   },
   {
     "title": "RE-SIP-E",
